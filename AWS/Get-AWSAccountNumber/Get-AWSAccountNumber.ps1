013a7f528bc06ad368eddbb82dacf6552c7462b8
Anthony Neace <hyperanthony@gmail.com>
2016-04-22 21:53:36 -0400

    Get-AWSAccountNumber: Initial Commit

---
+++ AWS/Get-AWSAccountNumber/Get-AWSAccountNumber.ps1
@@ -0,0 +1,14 @@
+# Get-AWSAccountNumber.ps1
+# --------------------------------------------------------------------------------
+# Get the stored profile's aws account number
+# 
+# USAGE:
+# ./Get-AWSAccountNumber.ps1
+#
+# EXAMPLE OUTPUT:
+# 111122223333
+# --------------------------------------------------------------------------------
+
+. ./lib.ps1
+
+Get-AWSAccountNumber
